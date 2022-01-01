cc00f40751b92803101236f5fc8ef67b80c426bc
regenrek <hello@regenrek.at>
2022-01-01 17:50:45 +0100

    updat test cases

--- .test/setup.ts
+++ .test/setup.ts
@@ -1,3 +1,22 @@
 import { vi } from 'vitest'
-global.vi = vi
-require('../vendor/jest-canvas-mock')
+global.jest = vi;
+import getCanvasWindow from 'jest-canvas-mock/lib/window';
+const apis = [
+    'Path2D',
+    'CanvasGradient',
+    'CanvasPattern',
+    'CanvasRenderingContext2D',
+    'DOMMatrix',
+    'ImageData',
+    'TextMetrics',
+    'ImageBitmap',
+    'createImageBitmap',
+] as const;
+
+const canvasWindow = getCanvasWindow({ document: window.document });
+
+apis.forEach((api) => {
+    global[api] = canvasWindow[api];
+    global.window[api] = canvasWindow[api];
+    document.head.innerHTML = '<div id="konva-container"></div>'
+});
--- test/canvas.test.ts
+++ test/canvas.test.ts
@@ -1,5 +1,5 @@
 import { describe, beforeEach, it, expect } from 'vitest'
-// import { useStage } from '../src/use/useStage'
+import { useStage } from '../src/use/useStage'
 
 let canvas: any;
 let ctx: CanvasRenderingContext2D;
@@ -9,7 +9,7 @@ beforeEach(() => {
     ctx = canvas.getContext('2d');
     canvas.width = 400;
     canvas.height = 300;
-    // ctx.setTransform(1, 2, 3, 4, 5, 6);
+    ctx.setTransform(1, 2, 3, 4, 5, 6);
 });
 
 describe('scale', () => {
@@ -22,32 +22,32 @@ describe('scale', () => {
         expect(ctx.scale).toBeCalled();
     });
 
-    // it('should scale the current transform', () => {
-    //     ctx.scale(2, 3);
-    //     expect(ctx.currentTransform).toEqual(new DOMMatrix([2, 4, 9, 12, 5, 6]));
-    // });
+    it('should scale the current transform', () => {
+        ctx.scale(2, 3);
+        expect(ctx.currentTransform).toEqual(new DOMMatrix([2, 4, 9, 12, 5, 6]));
+    });
 
     // it('should throw if argument count is less than 2', () => {
     //     expect(() => ctx.scale(1)).toThrow(TypeError);
     // });
 
-    // it('should not scale the transform if any of the values cannot be coerced into finite numbers', () => {
-    //     ctx.scale(1, Infinity);
-    //     expect(ctx.currentTransform).toEqual(new DOMMatrix([1, 2, 3, 4, 5, 6]));
+    it('should not scale the transform if any of the values cannot be coerced into finite numbers', () => {
+        ctx.scale(1, Infinity);
+        expect(ctx.currentTransform).toEqual(new DOMMatrix([1, 2, 3, 4, 5, 6]));
 
-    //     ctx.scale(NaN, 2);
-    //     expect(ctx.currentTransform).toEqual(new DOMMatrix([1, 2, 3, 4, 5, 6]));
-    // });
+        ctx.scale(NaN, 2);
+        expect(ctx.currentTransform).toEqual(new DOMMatrix([1, 2, 3, 4, 5, 6]));
+    });
 
-    // it('stage size should be window size', () => {
-    //     expect(window).toBeDefined();
-    //     const { initStage, stageDefaultConfig } = useStage();
+    it('stage size should be window size', () => {
+        expect(window).toBeDefined();
+        const { initStage, stageDefaultConfig } = useStage();
 
-    //     const stage = initStage(stageDefaultConfig);
+        const stage = initStage(stageDefaultConfig);
 
-    //     //expect(stage.width).toBe(window.innerWidth);
-    //     console.log("width", stage.width)
-    //     expect(stage.width).toBe(200);
-    //     expect(stage.height).toBe(window.innerHeight);
-    // });
+        console.log(stage)
+        
+        expect(stage.width()).toBe(window.innerWidth);
+        expect(stage.height()).toBe(window.innerHeight);
+    });
 });
